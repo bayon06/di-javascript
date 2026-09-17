ce3b40493c990e438da60af237f1582f567b6366
Alejandro <txa38306@educastur.es>
2026-09-17 18:47:05 +0200

    Resuelvo los ejercicios para pensar

--- src/01-variables/exercises.js
+++ src/01-variables/exercises.js
@@ -19,3 +19,26 @@ let precioBase=100;
 const IVA=0.21;
 let precioFinal=precioBase-(precioBase*IVA);
 console.log(precioFinal);
+
+
+/**
+ * PARA PENSAR
+ */
+let x = 1;
+{
+  let x = 2;
+  console.log(x);
+}
+console.log(x);
+// En la primera salida saldra un 2 y en la segunda un 1
+
+const lista = [1, 2];
+lista.push(3);
+console.log(lista);
+lista = [];
+// Por la consola saldría [1, 2, 3], falla porque al estar declarada la variable lista con const no se le puede reasignar otro valor
+
+let nombre2 = "Ana";
+let edad2 = 30;
+let tienePareja = true;
+let sueldo = 1250.5;
