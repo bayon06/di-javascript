dda6d90f07a53e94cdb325dbb86d932e41df2f68
“Alejandro <txa38306@educastur.es>
2026-09-17 13:29:02 +0200

    Realizo los ejercicios básicos

---
+++ src/01-variables/exercises.js
@@ -0,0 +1,21 @@
+/**
+ * BÁSICOS
+ */
+let nombre="Alejandro";
+console.log(nombre);
+nombre="Pepe";
+console.log(nombre);
+
+const PI=3.1416;
+//PI=67.67; Uncaught TypeError
+console.log(PI);
+
+let edad;
+console.log(edad);
+edad=20;
+console.log(edad);
+
+let precioBase=100;
+const IVA=0.21;
+let precioFinal=precioBase-(precioBase*IVA);
+console.log(precioFinal);
